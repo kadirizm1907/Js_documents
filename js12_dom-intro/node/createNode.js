2d32ddc98fa3fc0fb7982e8b316a8bff7325a87a
kadirizm1907 <kadiryurt1907@gmail.com>
2023-11-02 19:42:29 +0100

    z

--- js12_dom-intro/node/createNode.js
+++ js12_dom-intro/node/createNode.js
@@ -46,3 +46,34 @@ if(myPar.classList.contains("bg-danger")){
     myPar.classList.remove("bg-danger")
     myPar.classList.add("bg-warning")
 }
+
+myPar.classList.toggle("text-danger")
+//? toggle a yazilan deger varsa onu kaldirir. eger o deger yoksa o degeri ekler (class icine)
+
+//* ---------- Yeni element ekleme (innerHTML) yontemi
+
+const ul = document.querySelector("ul")
+ul.innerHTML += ` 
+<li id='deneme'> c++</li>
+<li class='fw-bold'>JAVA</li>
+<li> RUBY</li>
+<li> ASMBLY</li>
+`
+// ul.innerHTML += "<li> c++</li><li>JAVA</li><li> RUBY</li><li> ASMBLY</li>" //? CIFT TIRNAK ILE DE YAPILABILIR
+//? += YAPARSAK OLAN ELEMENLERIN ALTINA EKLER
+//? = DERSEK ESKI ELEMENTLERI SILER UZERNE YAZILA DEGERLERI EKLER
+
+
+
+const section3 = document.createElement("section")
+document.querySelector("main").appendChild(section3)
+
+section3.innerHTML = `
+<div class="card" style="width: 18rem;">
+  <img src="https://cdn.pixabay.com/photo/2023/10/27/12/13/vineyard-8345243_1280.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
