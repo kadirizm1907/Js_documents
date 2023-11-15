9f0cb6886f6142ecbe3159f533f0cb1db2cb38da
kadirizm1907 <kadiryurt1907@gmail.com>
2023-11-15 21:25:57 +0100

    fetch

--- async1/3-fetchAPI.js
+++ async1/3-fetchAPI.js
@@ -11,7 +11,28 @@
 //? bir parametre almaktadir ve bu istegin cevabini gosteren bir Promise dondurmektedir.
 
 console.log("FETCH")
-
+let veri = ""
 fetch("https://api.github.com/users")
- .then((res)=> res.json())
- .then((x)=>console.log(x))  
+ .then((res)=>{
+    console.log(res)
+    if(!res.ok){
+        throw new Error(`hata: ${res.status}`)
+    }
+    return res.json()
+ }) 
+ .then((data)=>{
+    veri = data
+show(data)})
+ .catch((err)=> console.log(err)) 
+
+ const show = (users) => {
+    const userSection = document.getElementById("users")
+    users.forEach((user) => {
+     userSection.innerHTML += `
+      <h1> ${user.avatar_url}</h1> 
+     ` 
+     
+    })
+    }
+    
+ 
