46dc2b9ad2ed3e72e3f5924cacf9bd0dfef3c3d7
kadirizm1907 <kadiryurt1907@gmail.com>
2023-11-20 19:27:21 +0100

    e

--- js_modules/browser_es_modules/hesapMakinasi.js
+++ js_modules/browser_es_modules/hesapMakinasi.js
@@ -1,15 +1,23 @@
-export const isim = "--hesap makinasi--";
-export const PI = 3.1415;
-export const asalSayilar = [2, 3, 5, 7, 9, 11, 13, 17, 19, 23]
+//*************************************/
+//* hesapMakinası.JS
+//*************************************/
 
-const topla = (a,b)=> a+b
-const cikar = (a,b)=> a-b
-const carp = (a,b)=> a*b
-const bol = (a,b)=> a/b
+//? named export
+export const isim = "-- Hesap Makinasi--"
 
-export {topla, cikar, bol, carp}
+export const PI = 3.1415
+export const asalSayilar = [2, 3, 5, 7, 11, 13, 17, 23]
 
-const mesaj = ()=> {
-    console.log("module bitti")
+const topla = (a, b) => a + b
+const cikar = (a, b) => a - b
+const carp = (a, b) => a * b
+const bol = (a, b) => a / b
+
+//? named export (Çoklu)
+export { topla, cikar, carp, bol }
+
+const mesaj = () => {
+  console.log("Module Bitti")
 }
-
+export default mesaj //? default export yöntemi
+//? modül başına sadece bir tane export default yapılabilir.
