b8dbdd04fe13a9ff5a99bd28f2afe0438ef14d05
kadirizm1907 <kadiryurt1907@gmail.com>
2023-11-20 19:04:13 +0100

    es module

---
+++ js_modules/browser_es_modules/app.js
@@ -0,0 +1,10 @@
+import { isim, PI } from "./hesapMakinasi.js";
+console.log(isim);
+console.log(PI * 10 * 20);
+console.log("toplam", topla(3, 4));
+console.log("bolum", bol(16, 4));
+console.log("carpim", carp(3, 4));
+console.log("cikar", cikar(31, 4));
+
+import { topla, cikar, bol, carp } from "./hesapMakinasi.js";
+
---
+++ js_modules/browser_es_modules/hesapMakinasi.js
@@ -0,0 +1,15 @@
+export const isim = "--hesap makinasi--";
+export const PI = 3.1415;
+export const asalSayilar = [2, 3, 5, 7, 9, 11, 13, 17, 19, 23]
+
+const topla = (a,b)=> a+b
+const cikar = (a,b)=> a-b
+const carp = (a,b)=> a*b
+const bol = (a,b)=> a/b
+
+export {topla, cikar, bol, carp}
+
+const mesaj = ()=> {
+    console.log("module bitti")
+}
+
