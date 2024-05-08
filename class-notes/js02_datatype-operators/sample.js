be3c5aba2ee996c8345d688946fd591639648594
kadirizm <kadiryurt1907@gmail.com>
2024-05-08 20:52:04 +0200

    new file

---
+++ class-notes/js02_datatype-operators/sample.js
@@ -0,0 +1,27 @@
+const number = 5
+
+{
+    const number = 10
+    console.log({ number });
+}
+console.log(number);
+
+let num = true
+
+console.log(num);
+{
+    let num = true
+    console.log(num);
+}
+
+console.log(typeof num);
+
+
+let counter
+counter = 55
+let id = 3
+console.log('counter:' + counter + " = " + "id:" + id);
+
+counter = counter + 5
+console.log(counter);
+
