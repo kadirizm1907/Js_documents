e7ad2648cf6f173db440909a22de4bcab4e4b6f1
kadirizm <kadiryurt1907@gmail.com>
2024-05-16 10:07:44 +0200

    traversing practice

--- class-notes/js12_dom-intro/traversingDOM.js
+++ class-notes/js12_dom-intro/traversingDOM.js
@@ -17,22 +17,24 @@ console.log("**** TRAVERSING DOM ****")
 //*----------------------------------------------
 //! parentElement, parentNode
 
-const htmlLi = document.querySelector("ul li:nth-child(1)")
-
-console.log(htmlLi.parentNode.parentNode.parentNode) //? main
-console.log(htmlLi.parentElement.parentElement.parentElement) //?main
+const htmlLi = document.querySelector(".item-list ul li:nth-child(2)")
+console.log(htmlLi.parentNode.parentNode.parentNode.parentElement); //? body
+console.log(htmlLi.parentElement); //? ul
 
 //? closest() metodu root yonundeki uygun olan parent'i secer
-console.log(htmlLi.closest("section")) //? en yakın section parent'ı bulur
+console.log(htmlLi.closest("section")) //? en yakın section parent'ı bulur -----section.item-list
 
-console.log(htmlLi.closest("main").querySelector("#add-new-item")) //? en yakın main'i bul icersindeki adnew-item idli elementi sec
+console.log(htmlLi.closest("main").querySelector("#btn")) //? en yakın main'i bul icersindeki btn id'li elementi sec ---<input id="btn" type="button" value="ADD">
 
-//* 2- Asagi Yonde traverse
-//*----------------------------------------------
+// //* 2- Asagi Yonde traverse
+// //*----------------------------------------------
 //? Children, firstElementChild, lastElementChild
 console.log(htmlLi.firstElementChild) //? Null
-console.log(htmlLi.firstChild) //? HTML
-console.log(htmlLi.lastChild) //? HTML
+console.log(htmlLi.firstChild) //? CSS
+console.log(htmlLi.lastChild) //? CSS
+const HtmlUl = document.querySelector(".item-list ul")
+console.log(HtmlUl.firstElementChild); //? HTML
+console.log(HtmlUl.firstChild); //? TEXT
 
 const itemList = document.querySelector(".item-list")
 console.log(itemList.firstElementChild) //?h3
@@ -41,12 +43,12 @@ console.log(itemList.lastElementChild) //? ul
 console.log(itemList.lastElementChild.firstElementChild) //? HTML li
 console.log(itemList.lastElementChild.lastElementChild) //? Django li
 
-//* 3- Yatay Yonde traverse
-//*----------------------------------------------
-//! nextElementSibling ,previousElementSibling
+// //* 3- Yatay Yonde traverse
+// //*----------------------------------------------
+// //! nextElementSibling ,previousElementSibling
 
-console.log(htmlLi.nextElementSibling.innerText)
-console.log(htmlLi.nextElementSibling.nextElementSibling.innerText)
+console.log(htmlLi.nextElementSibling.innerText)//? Javascript
+console.log(htmlLi.nextElementSibling.nextElementSibling.innerText)//? React
 console.log(
   htmlLi.nextElementSibling.nextElementSibling.previousElementSibling.innerText
-)
+)//? Javascript
